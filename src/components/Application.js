724471e84266cee1b09b2db42347385ebd22c8a5
Mimosa Poon <mimosapoon0703@gmail.com>
2016-10-03 11:18:28 +0800

    add web pack config, install babel, babel-loader, jquery

---
+++ src/components/Application.js
@@ -0,0 +1,8 @@
+import 'babel-polyfill';
+import $ from 'jquery';
+
+export default class Application {
+    init() {
+        console.log("Application init");
+    }
+}
---
+++ webpack.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    entry: './src/AppRenderer.js',
+    output: {
+        path: './build',
+        filename: 'app.bundle.js'
+    },
+
+    module: {
+        loaders:[
+            { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ }
+        ]
+    }
+};
